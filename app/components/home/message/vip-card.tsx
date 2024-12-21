cb3ef957682e68a05b9b190b19ebcb93f6cc3c59
AdibAhmed317 <ahmedadib317@gmail.com>
2024-12-21 11:35:13 +0600

    message section

--- app/components/home/message/vip-card.tsx
+++ app/components/home/message/vip-card.tsx
@@ -1,3 +1,19 @@
+// import { VipCardProps } from 'lib/types';
+
+// export const VipCard: React.FC<VipCardProps> = ({
+//   name,
+//   designation,
+//   imgSrc,
+// }) => {
+//   return (
+//     <div className='h-96 w-80 flex flex-col justify-center items-center shadow-2xl hover:shadow-xl transition-all rounded-md'>
+//       <img src={imgSrc} alt={`${name}'s photo`} className='px-10' />
+//       <h1 className='text-sm font-serif my-2'>{name}</h1>
+//       <h1 className='text-sm font-robotoBlack'>{designation}</h1>
+//     </div>
+//   );
+// };
+
 import { VipCardProps } from 'lib/types';
 
 export const VipCard: React.FC<VipCardProps> = ({
@@ -6,10 +22,14 @@ export const VipCard: React.FC<VipCardProps> = ({
   imgSrc,
 }) => {
   return (
-    <div className='h-96 w-80 flex flex-col justify-center items-center shadow-2xl hover:shadow-xl transition-all rounded-md'>
-      <img src={imgSrc} alt={`${name}'s photo`} className='px-10' />
-      <h1 className='text-sm font-serif my-2'>{name}</h1>
-      <h1 className='text-sm font-robotoBlack'>{designation}</h1>
+    <div className='h-96 w-full sm:w-80 flex flex-col justify-center items-center shadow-2xl hover:shadow-xl transition-all rounded-md bg-white p-4'>
+      <img
+        src={imgSrc}
+        alt={`${name}'s photo`}
+        className='w-32 h-32 rounded-full object-cover'
+      />
+      <h1 className='text-sm font-serif my-2 text-center'>{name}</h1>
+      <h1 className='text-sm font-robotoBlack text-center'>{designation}</h1>
     </div>
   );
 };
